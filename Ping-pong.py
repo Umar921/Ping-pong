2e441fccb09a1478e91f5945df30ef0d8aebab67
Umar921 <110150422+Umar921@users.noreply.github.com>
2022-08-02 10:47:34 +0500

    Update Ping-pong.py
    проиграть невозможно

--- Ping-pong.py
+++ Ping-pong.py
@@ -5,8 +5,10 @@ win_width = 700
 win_height = 500 
 window = display.set_mode((win_width,win_height))
 clock = time.Clock()
-display.set_caption('Shooter')
-backround = transform.scale(image.load('galaxy.jpg'),(win_width,win_height))
+display.set_caption('Pingpong')
+FPS = 60
+game = True
+finish = False
 
 class GameSprite(sprite.Sprite):
 #конструктор класса
@@ -32,10 +34,46 @@ mixer.music.play()
 game = True
 FPS = 60
 class Player(GameSprite):
-#метод для управления спрайтом стрелками клавиатуры
-    def update(self):
+    def update_l(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 400:
+            self.rect.y += self.speed
+    def update_r(self):
+            keys = key.get_pressed()
+            if keys[K_w] and self.rect.y > 5:
+                self.rect.y -= self.speed
+            if keys[K_s] and self.rect.y <  400:
+                self.rect.y += self.speed
+
+    
+
+racket1 = Player('racket.png', 5,win_height / 2, 30, 100 ,10)
+racket2 = Player('racket.png', win_width-30, win_height / 2, 30, 100, 10)
+
+speed_x = 3
+speed_y = 3
+ball = GameSprite('ball.png', 100, 100, 50, 50, 10)
+while game:
+    window.fill((200,255,255)) 
+    if not finish:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+        racket1.update_l()
+        racket2.update_r()
+        ball.update()
+
+    if ball.rect.y > win_height - 50 or ball.rect.y <=0:
+        speed_y *= -1
+    if sprite.collide_rect(racket1,ball) or sprite.collide_rect(racket2,ball):
+        speed_x *= -1
+
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    display.update()
+    clock.tick(60)
